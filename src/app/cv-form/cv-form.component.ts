6468f78d955c50b26e8bbff448868f1c3351ba50
Unknown <mikagarm@gmail.com>
2018-06-26 18:02:54 +0400

    jquery setup

--- src/app/cv-form/cv-form.component.ts
+++ src/app/cv-form/cv-form.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'app-cv-form',
   templateUrl: './cv-form.component.html',
   styleUrls: ['./cv-form.component.css']
 })
 export class CvFormComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
-   
+    jQuery('.ui.radio.checkbox')
+      .checkbox();
   }
 
 }
