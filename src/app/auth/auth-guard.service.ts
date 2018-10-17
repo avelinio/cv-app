3a9c60b26877bc7e98b078f79dd9e0c968159fef
Vahagn Yavruyan <vahagyavruyan@gmail.com>
2018-10-17 16:56:09 +0400

    guard for list module

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,15 +1,18 @@
 import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
+  CanActivate, Router
 } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.isAuthenticated();
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate() {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/signin']);
+    }
+    return false;
   }
 }
